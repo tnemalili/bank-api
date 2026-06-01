540ad7a7161017024f1ba0d5e0cde82507970e34
tshaks <tnemalili@gmail.com>
2026-06-01 10:14:18 +0200

    Get started messaging

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.24
 toolchain go1.24.2
 
 require (
+	github.com/aws/aws-sdk-go-v2/config v1.32.20
+	github.com/aws/aws-sdk-go-v2/service/sns v1.39.19
 	github.com/gofiber/fiber/v2 v2.52.6
 	github.com/sirupsen/logrus v1.8.1
 	gorm.io/driver/sqlite v1.6.0
@@ -13,6 +15,19 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.41.9 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.19.19 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.18.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.4.26 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.25 // indirect
+	github.com/aws/aws-sdk-go-v2/service/signin v1.1.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.30.19 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.36.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.42.3 // indirect
+	github.com/aws/smithy-go v1.26.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
--- messaging/mclient.go
+++ messaging/mclient.go
@@ -1,19 +1,62 @@
 package messaging
 
-import "github.com/sftx/bank-api/core/ports"
+import (
+	"context"
+	"encoding/json"
+	"os"
 
-type MessaginClient struct {}
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+	"github.com/sftx/bank-api/core/ports"
+	log "github.com/sirupsen/logrus"
+)
 
-func (c *MessaginClient) Publish(topic string, event any) error {
-	panic("unimplemented")
+type messagingClient struct {
+	snsClient *sns.Client
+}
+
+func (c *messagingClient) Publish(topic string, event any) error {
+	// Serialize the event to JSON
+	message, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+	// Publish the message to the specified SNS topic
+	pubOutput, err := c.snsClient.Publish(context.TODO(), &sns.PublishInput{
+		Message:  aws.String(string(message)),
+		TopicArn: aws.String(topic),
+	})
+	if err != nil {
+		log.Printf("failed to publish message to SNS topic %s: %v", topic, err)
+		return err
+	}
+	log.Printf("message published to SNS topic %s with message ID %s", topic, *pubOutput.MessageId)
+	return nil
 }
 
 
-func NewMessagingClient() *MessaginClient {
+func NewMessagingClient() *messagingClient {
+	
+	ctx := context.Background()
+	
+	region := os.Getenv("AWS_REGION")
+	// Default to us-east-1 if AWS_REGION is not set
+	if region == "" {
+		region = "us-east-1"
+	}
 
-	return &MessaginClient{}
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
+	if err != nil {
+		log.Errorf("Failed to load SDK configuration, %v", err)
+	}
+
+	// Create an Amazon SNS client
+	snsClient := sns.NewFromConfig(cfg)
+
+	return &messagingClient{snsClient: snsClient}
 }
 
 
 // Get feedback in case we are not implementing the interface correctly
-var _ ports.IMessagingService = (*MessaginClient)(nil)
+var _ ports.IMessagingService = (*messagingClient)(nil)
